292fe7007dcda5bad39198bbb93b8b1e01a3a85f
trudieis <trudieis@student.21-school.ru>
2023-04-20 22:15:59 +0300

    add 2nd answer

--- src/02/main.sh
+++ src/02/main.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-./script.sh
+REGEX="^[Yy]"
+
+if [ $# -ne 0 ]
+    then
+        echo "Параметры не нужны!"
+else 
+    ./script.sh
+    read -p "Хотите сохранить данные в файл? " input
+    if [[ $input =~ $REGEX ]] ; then           
+            ./script.sh >$(date +"%d_%m_%y_%H_%M_%S").status
+    fi
+fi
