dab8701693f565327eaeab16a59077599452f531
trudieis <trudieis@student.21-school.ru>
2023-04-25 16:22:03 +0300

    clean script

--- src/03/main.sh
+++ src/03/main.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]
+if [ $# -eq 4 ]
+  then
+    BG="$1"
+    SR="$2"
+    BG_S="$3"
+    SR_S="$4"
+elif [ $# -ne 4 ]
     then
         echo "Мало параметров!"
     # Сделать ду вайл пока не еквал 4 параметра и 1 и 2 разные как и 3 и 4 разные
-else 
-    ./script.sh $1 $2 $3 $4
+    while [ $# -ne 4 ] ; do
+       read -r -p "введите параметры фона и шрифта" $BG $SR $BG_S $SR_S
+    done
 fi
+./script.sh $BG $SR $BG_S $SR_S
