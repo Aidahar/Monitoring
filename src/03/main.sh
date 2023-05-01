c9770731e28ec4b87fd21580ee6b203de9c6e1ce
trudieis <trudieis@student.21-school.ru>
2023-05-01 23:00:44 +0300

    change os

--- src/03/main.sh
+++ src/03/main.sh
@@ -2,18 +2,17 @@
 
 if [ $# -eq 4 ]
   then
-    BG="$1"
-    SR="$2"
-    BG_S="$3"
-    SR_S="$4"
+    NAME="$1"
+    BG="$2"
+    N_SEC="$3"
+    BG_SEC="$4"
 elif [ $# -ne 4 ]
     then
         echo "Мало параметров!"
     # Сделать ду вайл пока не еквал 4 параметра и 1 и 2 разные как и 3 и 4 разные
-     while [ $# -ne 4 ] ; do
-         echo "Enter 4 parameters"
-         exit 1
-     done
+    while [[ $# -ne 4 ]] ; do
+        read -r -p "Введите 4 параметра " NAME BG N_SEC BG_SEC
+    done
 fi
 
-./colors.sh $BG $SR $BG_S $SR_S
+./colors.sh $NAME $BG $N_SEC $BG_SEC
