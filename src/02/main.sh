bc7ad434a7c3dce277d2c6d3187a8de05a1f48ad
trudieis <trudieis@student.21-school.ru>
2023-05-01 11:34:39 +0300

    change name data

--- src/02/main.sh
+++ src/02/main.sh
@@ -6,9 +6,9 @@ if [ $# -ne 0 ]
     then
         echo "Параметры не нужны!"
 else 
-    ./script.sh
+    ./data.sh
     read -p "Хотите сохранить данные в файл? " input
     if [[ $input =~ $REGEX ]] ; then           
-            ./script.sh >$(date +"%d_%m_%y_%H_%M_%S").status
+            ./data.sh >$(date +"%d_%m_%y_%H_%M_%S").status
     fi
 fi
