23646612e1eae27971a67015e86da4de316d9540
trudieis <trudieis@student.21-school.ru>
2023-05-01 11:28:27 +0300

    change hostname

--- src/03/main.sh
+++ src/03/main.sh
@@ -10,8 +10,10 @@ elif [ $# -ne 4 ]
     then
         echo "Мало параметров!"
     # Сделать ду вайл пока не еквал 4 параметра и 1 и 2 разные как и 3 и 4 разные
-    while [ $# -ne 4 ] ; do
-       read -r -p "введите параметры фона и шрифта" $BG $SR $BG_S $SR_S
-    done
+     while [ $# -ne 4 ] ; do
+         echo "Enter 4 parameters"
+         exit 1
+     done
 fi
+
 ./script.sh $BG $SR $BG_S $SR_S
